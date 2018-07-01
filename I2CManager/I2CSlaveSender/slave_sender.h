47626053f2a4dd54bcadd46a64a0fdc6c08abc11
nikiryota0731 <35524179+nikiryota0731@users.noreply.github.com>
2018-07-01 18:52:01 +0900

    コメントの追加
    コメントを追加しました。

--- I2CManager/I2CSlaveSender/slave_sender.h
+++ I2CManager/I2CSlaveSender/slave_sender.h
@@ -1,3 +1,8 @@
+/*
+  @progma I2C通信でslaveがmasterに送るクラス
+  @date 2018/07/01
+*/
+
 #pragma once
 #include <arduino.h>
 #include <Wire.h>
@@ -10,6 +15,7 @@ class SlaveSender {
 
   public:
     slaveSender(int arraySize);
+  // デストラクタ
     virtual ~slaveSender() {
       delete[] data;
     }
@@ -20,30 +26,51 @@ class SlaveSender {
     void show();
 };
 
+/*
+  コンストラクタ
+  @param arraySize 送信するデータの個数
+*/
 slaveSender::slaveSender(int arraySize) {
   data = new byte[arraySize];
   Size = arraySize;
 }
 
+/*
+  指定した配列にデータをセット
+  @param arrayNum 何番目の配列か
+  @param Data 送信する値(0-255)
+*/
 void slaveSender::setData(int arrayNum, byte Data) {
   Data = constrain(Data, 0, 255);
   data[arrayNum - 1] = Data;
 }
 
+/*
+  指定した配列の指定したbitにデータをセット
+  @param arrayNum 何番目の配列か
+  @param BitNum   何番目のbitか
+  @param Data     送信する値(1or0)
+*/
 void slaveSender::setBitData(int arrayNum, int BitNum, bool Data) {
   Data = (bool)Data;
   bitWrite(data[arrayNum-1], BitNum - 1, Data);
 }
 
+//送るデータのリセット
 void slaveSender::Reset() {
   for (int i = 0; i < Size; i++)
     data[i] = 0;
 }
 
+/*
+  masterに送信
+  requestEvent内で実行
+*/
 void slaveSender::Update() {
   Wire.write(data, Size);
 }
 
+//送信するデータの表示
 void slaveSender::show() {
   for (int i = 0; i < Size; i++) {
     Serial.print(data[i]);
