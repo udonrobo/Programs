993fc4c803eb98bccb50921ed531aed1bdc36b93
nikiryota0731 <35524179+nikiryota0731@users.noreply.github.com>
2018-07-01 18:38:26 +0900

    コメントの追加
    コメントを追加しました。

--- I2CManager/I2CMasterReader/master_reader.h
+++ I2CManager/I2CMasterReader/master_reader.h
@@ -1,3 +1,9 @@
+/*
+  @program I2C通信でmasterがslaveからデータを受け取るクラス
+  @data 2018/07/01
+*/
+
+
 #pragma once
 #include <Arduino.h>
 #include <Wire.h>
@@ -10,6 +16,7 @@ class MasterReader {
 
   public:
     masterReader(int Address, int arraySize);
+  // デストラクタ
     virtual ~masterReader() {
       delete[] data;
     }
@@ -19,7 +26,11 @@ class MasterReader {
     void show();
 };
 
-
+/*
+  コンストラクタ
+  @param Address I2Cアドレス
+  @param arraySize 送信するデータ個数
+*/
 masterReader::masterReader(int Address, int arraySize) {
   data = new byte[arraySize];
   for (int i = 0; i < arraySize; i++) 
@@ -28,6 +39,7 @@ masterReader::masterReader(int Address, int arraySize) {
   i2cAddress = Address;
 }
 
+//slave側から受け取り
 void masterReader::Update() {
   Wire.requestFrom(i2cAddress, Size);
   while (Wire.available()) {
@@ -36,14 +48,24 @@ void masterReader::Update() {
   }
 }
 
+/*
+  送られてきたデータの取得
+  @param 何個目のデータか
+*/
 byte masterReader::getData(int arrayNum) {
   return data[arrayNum - 1];
 }
 
+/*
+  送られてきたデータのbitの取得
+  @param 何個目のデータか
+  @param 何個目のbitか
+*/
 bool masterReader::getBitData(int arrayNum, int bitNum) {
   return bitRead(data[arrayNum], bitNum);
 }
 
+//送られてきたデータの全ての表示
 void masterReader::show() {
   for (int i = 0; i < Size; i++) {
     Serial.print(data[i]);
